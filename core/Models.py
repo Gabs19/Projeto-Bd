d3581885940291ee99151ae2d6b35f421b9d041d
William Teles <teles967@gmail.com>
2021-02-16 16:46:09 -0300

    Arrumando funções CRUD, alguns atributos e indeces

--- core/Models.py
+++ core/Models.py
@@ -3,25 +3,39 @@ from tkinter.constants import CASCADE, TRUE
 from typing import cast
 from mongoengine import *
 
+
 connect('mongobd_concessionaria')
 
-class Vendedor(Document):
-    nome = StringField(max_length=50,required=True)
-    codigo_vendedor = StringField(max_length=10,required=True,unique=True)
 
-    def criar(self,nome,codigo_vendedor):
+class Vendedor(Document):
+    nome = StringField(max_length=50, required=True)
+    cpf = StringField(max_length=11, required=True, unique=True)
+
+    def criar(self, nome, cpf):
         self.nome = nome
-        self.codigo_vendedor = codigo_vendedor
+        self.cpf = cpf
         self.save()
 
-    def ler(self):
-        vendedor = Vendedor.objects(codigo_vendedor = '1203456879')
-        for i in vendedor:
-            print(f'nome : {i.nome}\ncodigo : {i.codigo_vendedor}')
-
-    def atualizar(self,nome_editado):
+    def ler(self, cpf_busca):
         try:
-            vendedor = Vendedor.objects(codigo_vendedor = '1203456879').get()
+            vendedor = Vendedor.objects(cpf = cpf_busca)
+            print('\tVendedor:')
+            for i in vendedor:
+                print(f'nome: {i.nome}\ncodigo: {i.cpf}')
+        except DoesNotExist:
+            print('Not Found')
+
+        try:
+            carros = Carro.objects(vendedor = cpf_busca)
+            print('\tCarros:')
+            for i in carros:
+                print(f'modelo: {i.modelo}\nplaca: {i.placa}')
+        except DoesNotExist:
+            print('Não possui carros')
+
+    def atualizarNome(self, nome_editado, cpf_busca):
+        try:
+            vendedor = Vendedor.objects(cpf = cpf_busca).get()
             vendedor.update(
                 nome = nome_editado
             )
@@ -29,37 +43,48 @@ class Vendedor(Document):
             print('Editado')
         except DoesNotExist:
             print("User not found")
-        
 
-    def deletar(self):
+    def deletar(self, cpf_busca):
         try:
-            Vendedor.objects(codigo_vendedor = '1203456879').delete()
-            print('deletaoo')
+            Vendedor.objects(cpf = cpf_busca).delete()
+            print('deletado')
         except DoesNotExist:
             print("User not found")
 
     meta = {
-        'indexes' : ['codigo_vendedor']
+        'indexes' : ['cpf']
     }
 
-    
-class Comprador(Document):
-    nome = StringField(max_length=50,required=True)
-    cpf = StringField(max_length=11,required=True,unique=True)
 
-    def criar(self,nome,cpf):
+class Comprador(Document):
+    nome = StringField(max_length=50, required=True)
+    cpf = StringField(max_length=11, required=True, unique=True)
+
+    def criar(self, nome, cpf):
         self.nome = nome
         self.cpf = cpf
         self.save()
 
-    def ler(self):
-        comprador = Comprador.objects(cpf = '12034568791')
-        for i in comprador:
-            print(f'nome : {i.nome}\ncpf : {i.cpf}')
-
-    def atualizar(self,nome_editado):
+    def ler(self, cpf_busca):
         try:
-            comprador = Comprador.objects(cpf = '12034568791').get()
+            comprador = Comprador.objects(cpf = cpf_busca)
+            print('\tComprador:')
+            for i in comprador:
+                print(f'nome : {i.nome}\ncpf : {i.cpf}')
+        except DoesNotExist:
+            print('Not found')
+
+        try:
+            carros = Carro.objects(comprador = cpf_busca)
+            print('\tCarros:')
+            for i in carros:
+                print(f'modelo: {i.modelo}\nplaca: {i.placa}')
+        except DoesNotExist:
+            print('Não possui carros')
+
+    def atualizarNome(self, nome_editado, cpf_busca):
+        try:
+            comprador = Comprador.objects(cpf = cpf_busca).get()
             comprador.update(
                 nome = nome_editado
             )
@@ -67,80 +92,90 @@ class Comprador(Document):
             print('Editado')
         except DoesNotExist:
             print("User not found")
-        
 
-    def deletar(self):
+    def deletar(self, cpf_busca):
         try:
-            Comprador.objects(cpf = '12034568791').delete()
-            print('deletaoo')
+            Comprador.objects(cpf = cpf_busca).delete()
+            print('deletado')
         except DoesNotExist:
             print("User not found")
 
     meta = {
         'indexes' : ['cpf']
     }
-    
-class Carro(DynamicDocument):
-    modelo = StringField(unique=True,required=True)
-    valor = StringField(required=True)
-    dono = ReferenceField(Comprador,CASCADE=True)
-    vendedor = ReferenceField(Vendedor, CASCADE=True)
 
-    def criar(self,modelo,valor,dono,vendedor):
+
+class Carro(DynamicDocument):
+    modelo = StringField(unique=True, required=True)
+    valor = StringField(required=True)
+    comprador = ListField(ReferenceField(Comprador, CASCADE=True))
+    vendedor = ListField(ReferenceField(Vendedor, CASCADE=True))
+    placa = StringField(max_length=7, required=True, unique=True)
+
+    def criar(self, modelo, valor, placa, comprador, vendedor):
         self.modelo = modelo
         self.valor = valor
-        self.dono = dono
+        self.placa = placa
+        self.comprador = comprador
         self.vendedor = vendedor
         self.save()
 
-    def ler(self):
-        carro = Carro.objects(modelo = 'BMW')
-        for i in carro:
-            print(f'nome : {i.modelo}\nvalor : {i.valor}\ndono: {i.dono}. \n Vendido por {i.vendedor}')
+    def ler(self, placa_busca):
+        try
+            carro = Carro.objects(placa = placa_busca)
+            for i in carro:
+                print(f'nome : {i.modelo}\nvalor : {i.valor}\ndono: {i.comprador}. \n Vendido por {i.vendedor}')
+        except:
+            print('Not found')
 
-    def atualizar(self,modelo_editado,valor_editado):
+    def atualizarModelo(self, modelo_editado, placa_busca):
         try:
-            carro = Carro.objects(modelo = 'BMW').get()
+            carro = Carro.objects(placa = placa_busca).get()
             carro.update(
-                modelo = modelo_editado,
+                modelo = modelo_editado
+            )
+            carro.reload()
+            print('Editado')
+        except DoesNotExist:
+            print("User not found")
+
+    def atualizarValor(self, valor_editado, placa_busca):
+        try:
+            carro = Carro.objects(placa = placa_busca).get()
+            carro.update(
                 valor = valor_editado
             )
             carro.reload()
             print('Editado')
         except DoesNotExist:
             print("User not found")
-        
 
-    def deletar(self):
+    def deletar(self, placa_busca):
         try:
-            Carro.objects(modelo = 'BMW').delete()
-            print('deletaoo')
+            Carro.objects(placa = placa_busca).delete()
+            print('deletado')
         except DoesNotExist:
             print("User not found")
 
-
     meta = {
-        'indexes' : ['modelo']
-    }
+        'indexes' : ['placa']
 
 
 vendedor = Vendedor()
-vendedor.criar('jacquan','1203456879') 
-# vendedor.atualizar('erick jaquin')
-# vendedor.ler()
-# vendedor.deletar()
-
+vendedor.criar('jacquan', '12034568791') 
+# vendedor.atualizarNome('erick jaquin', '12034568791')
+# vendedor.ler('12034568791')
+# vendedor.deletar('12034568791')
 
 comprador = Comprador()
-comprador.criar('william','12034568791') 
-# comprador.atualizar('willis')
-# comprador.ler()
-# comprador.deletar()
+comprador.criar('william', '12034568790') 
+# comprador.atualizarNome('willis', '12034568790')
+# comprador.ler('12034568790')
+# comprador.deletar('12034568790')
 
 carro = Carro()
-carro.criar('BMW', '5000,00',comprador = comprador, vendedor = vendedor)
-# # carro.atualizar('AUDI','7000,00')
-# carro.ler()
-# carro.deletar()
-
-
+carro.criar('BMW', '5000,00', 'ABC1234', comprador.cpf, vendedor.cpf)
+# carro.atualizarModelo('AUDI', 'ABC1234')
+# carro.atualizarModelo('7000,00', 'ABC1234')
+# carro.ler('ABC1234')
+# carro.deletar('ABC1234')
